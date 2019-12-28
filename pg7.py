79bf133ed47112570fca1b0cced4d7e979ea2d56
Maria <andreeva-mm03@yandex.ru>
2019-12-28 13:55:45 +0300

    Перемещение героя. Дополнительные уровни

--- pg7.py
+++ pg7.py
@@ -17,7 +17,7 @@ def terminate():
     sys.exit()
 
 name = input()
-if name == '':
+if not os.path.isfile("data/" + name):
     print('Название карты не было введено')
     terminate()
 
@@ -34,14 +34,12 @@ running = True
 
 def load_level(filename):
     filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
+
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
 
-    # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
 
-    # дополняем каждую строку пустыми клетками ('.')
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
